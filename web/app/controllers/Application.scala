1dc0f8a56d9c4587566c85f460a322fc64c6d7d4
Christophe Marchal <toff63@gmail.com>
2015-06-04 18:24:25 -0300

    Create route using actor
    Created a route that call a simple hello world actor. The actor is
    created locally. I followed the first example from
    https://www.playframework.com/documentation/2.4.x/ScalaAkka

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -1,12 +1,44 @@
 package controllers
 
+import akka.actor._
 import play.api._
 import play.api.mvc._
+import javax.inject._
+import akka.pattern.ask
+import scala.concurrent.duration._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import akka.util.Timeout
 
-class Application extends Controller {
+trait AsyncConfiguration {
+  val timeout:Timeout = 5 seconds
+}
+
+@Singleton
+class Application @Inject() (system: ActorSystem) extends Controller with AsyncConfiguration {
+  val helloActor = system.actorOf(HelloActor.props, "hello-actor")
+  val threadPool = play.api.libs.concurrent.Execution.Implicits.defaultContext
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("Helllo from Play 4.1")
+  }
+  
+  def hello = Action.async {
+    helloActor.ask(HelloActor.SayHello("World"))(timeout).mapTo[String].map {message => Ok(message)}
   }
 
+}
+
+object HelloActor {
+  def props = Props[HelloActor]
+
+  case class SayHello(name: String)
+}
+
+class HelloActor extends Actor {
+  import HelloActor._
+
+  def receive = {
+    case SayHello(name: String) =>
+      sender() ! "Hello, " + name
+  }
 }
